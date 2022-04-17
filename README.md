55383c7ae88bde3bcf273155d7c4aec4e37db0c9
ArmanZarrin97 <armanzarrin97@gmail.com>
2022-04-18 01:29:27 +0430

    add argument

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Organize_Directory
 It takes a Directory and moves similar files based their type to several folders
+
+#   How to run
+
+    1- Add src to Python Path
+        "
+         export PYTHONPATH=${PWD}
+        "
+    2- Run with replacing your directory
+
+        "
+        python src/clean.py $Your Directory$
+        "
--- src/clean.py
+++ src/clean.py
@@ -1,7 +1,8 @@
 import json
+import shutil
+import sys
 from pathlib import Path
 
-import shutil
 from loguru import logger
 
 from src.data import Data_DIR
@@ -12,10 +13,7 @@ class Organizefiles:
       """
     It takes a directory path and oraganize tha containing files in specific destinations
     """
-      def __init__(self , Dir_Path):
-          self.path=Path(Dir_Path);
-          if not self.path.exists():
-                raise FileNotFoundError("The path is not valid");
+      def __init__(self):
           self.extensions_dest={};
           with open(Data_DIR/'extensions.json') as f:
                 ext_dirs=json.load(f);
@@ -23,13 +21,16 @@ class Organizefiles:
                       for extension in extensions:
                             self.extensions_dest[extension]=Dir_name;
           #print(self.extensions)
-      def __call__(self):
-            logger.info(f"Organizing files in {self.path}...");
+      def __call__(self, Dir_Path):
+            Dir_Path=Path(Dir_Path);
+            if not Dir_Path.exists():
+                    raise FileNotFoundError("The path is not valid");
+            logger.info(f"Organizing files in {Dir_Path}...");
             """
             organizes the files in the directory by moving them to their respective directories
             """
             file_extensions=[]
-            for file_path in self.path.iterdir():
+            for file_path in Dir_Path.iterdir():
                 #ignore_directories
                 if file_path.is_dir():
                     continue
@@ -38,14 +39,15 @@ class Organizefiles:
                     continue
                 file_extensions.append(file_path.suffix)
                 if file_path.suffix not in self.extensions_dest:
-                    Dest_Dir=self.path/'other'
+                    Dest_Dir=Dir_Path/'other'
                 else:
-                    Dest_Dir=self.path/self.extensions_dest[file_path.suffix]
+                    Dest_Dir=Dir_Path/self.extensions_dest[file_path.suffix]
                 Dest_Dir.mkdir(exist_ok=True)
                 shutil.move(str(file_path),str(Dest_Dir))
                 logger.info(f'{file_path} moved to {Dest_Dir}')
 
 if __name__ == "__main__":
-  org_files=Organizefiles('/home/arman/Downloads');
-  org_files();
-  logger.info("Done!");
+    logger.info(f"your Input Directory is: {sys.argv[1]}");
+    org_files=Organizefiles();
+    org_files(sys.argv[1]);
+    logger.info("Done!");
